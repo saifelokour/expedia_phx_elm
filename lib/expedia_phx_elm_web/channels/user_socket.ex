5a1ea9d9dec7345bc1caadfb3a4f51d308ac48fe
Saif Elokour <saiffelokour@gmail.com>
2018-01-11 01:00:40 +0200

    Use production config from Heroku ENV variables and decrease socket timeout

--- lib/expedia_phx_elm_web/channels/user_socket.ex
+++ lib/expedia_phx_elm_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule ExpediaPhxElmWeb.UserSocket do
   # channel "room:*", ExpediaPhxElmWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
