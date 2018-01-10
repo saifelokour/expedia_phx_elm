fe4c76dc5f5a63596d0af03e381e2848a9a39dbb
Saif Elokour <saiffelokour@gmail.com>
2018-01-10 20:31:50 +0200

    adds multiple query test

--- test/expedia_phx_elm_web/controllers/page_controller_test.exs
+++ test/expedia_phx_elm_web/controllers/page_controller_test.exs
@@ -8,10 +8,21 @@ defmodule ExpediaPhxElmWeb.OffersControllerTest do
     assert dest["regionID"] == "178286"
   end
 
-  test "GET /api/offers with query on destinationName", %{conn: conn} do
+  test "GET /api/offers with with single query", %{conn: conn} do
     conn = get conn, "/api/offers?destinationName=LA"
     %{"offers" => %{"Hotel" => [ %{"destination" => dest} | _]}} = json_response(conn, 200)
 
+    assert dest["regionID"] == "178280"
+  end
+
+  test "GET /api/offers with multiple queries", %{conn: conn} do
+    conn = get conn, "/api/offers?destinationName=LA&lengthOfStay=1"
+    %{"offers" => %{
+        "Hotel" => [ %{"offerDateRange" => range, "destination" => dest} | _]
+        }
+      } = json_response(conn, 200)
+
+    assert range["lengthOfStay"] == 1
     assert dest["regionID"] == "178280"
   end
 end
