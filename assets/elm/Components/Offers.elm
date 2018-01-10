1af73cfcbd3ef2532bcccba094ab443ef77e602b
Saif Elokour <saiffelokour@gmail.com>
2018-01-10 23:29:30 +0200

    creates offers model fetches data from api and creates list view

---
+++ assets/elm/Components/Offers.elm
@@ -0,0 +1,90 @@
+module Components.Offers exposing (..)
+import Html exposing (Html, text, ul, li, div, h2, button)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+import List
+import Hotel exposing (..)
+import Http
+import Json.Decode as Json exposing (..)
+import Debug
+
+type alias Model =
+  { offers: List Hotel.Model }
+
+type Msg
+  = NoOp
+  | Fetch
+  | UpdateOffers (Result Http.Error (List Hotel.Model))
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NoOp ->
+      (model, Cmd.none)
+    Fetch ->
+      (model, fetchOffers)
+    UpdateOffers result ->
+      case result of
+        Ok offers ->
+          (Model offers, Cmd.none)
+        Err error ->
+          case error of
+            Http.BadPayload errorMessage x ->
+                Debug.log errorMessage
+                (model, Cmd.none)
+            _ ->
+                (model, Cmd.none)
+
+-- HTTP calls
+fetchOffers : Cmd Msg
+fetchOffers =
+  let
+    url = "/api/offers"
+  in
+    Http.send UpdateOffers (Http.get url decodeOffersFetch)
+
+-- Fetch the offers out of the "offers" key
+decodeOffersFetch : Json.Decoder (List Hotel.Model)
+decodeOffersFetch =
+  Json.at ["offers"] decodeHotelsFetch
+
+-- Fetch the hotels out of the "hotel" key
+decodeHotelsFetch : Json.Decoder (List Hotel.Model)
+decodeHotelsFetch =
+  Json.at ["Hotel"] decodeHotelList
+
+-- Then decode the "hotel" key into a List of Hotel.Models
+decodeHotelList : Json.Decoder (List Hotel.Model)
+decodeHotelList =
+  Json.list decodeHotelData
+
+-- Finally, build the decoder for each individual Hotel.Model
+decodeHotelData : Json.Decoder Hotel.Model
+decodeHotelData = 
+  Json.map6 
+    Hotel.Model
+    (Json.at ["hotelInfo"] Hotel.decodeHotelInfo)
+    (Json.at ["hotelUrls"] Hotel.decodeHotelUrls)
+    (Json.at ["destination"] Hotel.decodeDestination)
+    (Json.at ["hotelUrgencyInfo"] Hotel.decodeHotelUrgencyInfo)
+    (Json.at ["hotelPricingInfo"] Hotel.decodeHotelPricingInfo)
+    (Json.at ["offerDateRange"] Hotel.decodeOfferDateRange)
+
+renderOffer : Hotel.Model -> Html a
+renderOffer hotel =
+  li [ ] [ Hotel.view hotel ]
+
+renderOffers : Model -> List (Html a)
+renderOffers offers =
+  List.map renderOffer offers.offers
+
+initialModel : Model
+initialModel =
+  {offers = []}
+      
+
+view : Model -> Html Msg
+view model =
+  div [ class "Offers-list" ]
+    [ h2 [] [ text "Offers List" ]
+    , ul [] (renderOffers model) ]
