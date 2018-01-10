ec7dd0213085056836ecb1568017805e27d3fd11
Saif Elokour <saiffelokour@gmail.com>
2018-01-10 23:28:03 +0200

    create hotel model and decoders with basic view

---
+++ assets/elm/Components/Hotel.elm
@@ -0,0 +1,103 @@
+module Hotel exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Json.Decode as Json exposing (..)
+import Http exposing (..)
+
+type alias Model =
+  { hotelInfo : HotelInfo
+  , hotelUrls : HotelUrls
+  , destination : Destination
+  , hotelUrgencyInfo : HotelUrgencyInfo
+  , hotelPricingInfo : HotelPricingInfo
+  , offerDateRange : OfferDateRange
+  }
+
+type alias OfferDateRange =
+  { lengthOfStay : Int
+  , travelStartDate : List Int
+  , travelEndDate : List Int
+  }
+
+decodeOfferDateRange : Json.Decoder OfferDateRange
+decodeOfferDateRange = 
+  Json.map3 
+    OfferDateRange
+    (Json.at ["lengthOfStay"] Json.int)
+    (Json.at ["travelStartDate"] (Json.list int))
+    (Json.at ["travelEndDate"] (Json.list int))
+
+type alias Destination =
+  { regionID : String
+  , longName : String
+  }
+
+decodeDestination : Json.Decoder Destination
+decodeDestination = 
+  Json.map2
+    Destination
+    (Json.at ["regionID"] Json.string)
+    (Json.at ["longName"] Json.string)
+
+type alias HotelInfo =
+  { hotelName : String
+  , hotelStarRating : String
+  , hotelImageUrl : String
+  }
+
+decodeHotelInfo : Json.Decoder HotelInfo
+decodeHotelInfo = 
+  Json.map3 
+    HotelInfo
+    (Json.at ["hotelName"] Json.string)
+    (Json.at ["hotelStarRating"] Json.string)
+    (Json.at ["hotelImageUrl"] Json.string)
+
+type alias HotelUrgencyInfo =
+  { numberOfRoomsLeft : Int
+  , almostSoldStatus : String
+  }
+
+decodeHotelUrgencyInfo : Json.Decoder HotelUrgencyInfo
+decodeHotelUrgencyInfo = 
+  Json.map2
+    HotelUrgencyInfo
+    (Json.at ["numberOfRoomsLeft"] Json.int)
+    (Json.at ["almostSoldStatus"] Json.string)
+
+type alias HotelPricingInfo =
+  { originalPricePerNight : Float
+  , currency : String
+  }
+
+decodeHotelPricingInfo : Json.Decoder HotelPricingInfo
+decodeHotelPricingInfo = 
+  Json.map2
+    HotelPricingInfo
+    (Json.at ["originalPricePerNight"] Json.float)
+    (Json.at ["currency"] Json.string)
+
+
+type alias HotelUrls =
+  { hotelInfositeUrl : String}
+
+decodeHotelUrls : Json.Decoder HotelUrls
+decodeHotelUrls = 
+  Json.map 
+    HotelUrls
+    (Json.at ["hotelInfositeUrl"] Json.string)
+
+view : Model -> Html a
+view model =
+  case Http.decodeUri model.hotelUrls.hotelInfositeUrl of
+      Just url ->
+          hotelView model url
+      Nothing ->
+        div [] []
+
+hotelView : Model -> String -> Html a
+hotelView model url =
+  div [ class "hotel" ] [
+        a [ href url ] [ img [src model.hotelInfo.hotelImageUrl] [] ]
+      , strong [] [text model.hotelInfo.hotelName]
+      ] 
