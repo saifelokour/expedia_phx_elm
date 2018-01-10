c0ff881ce1913c725a225d902c3101df6dba058a
Saif Elokour <saiffelokour@gmail.com>
2018-01-10 20:16:37 +0200

    adds base test

--- test/expedia_phx_elm_web/controllers/page_controller_test.exs
+++ test/expedia_phx_elm_web/controllers/page_controller_test.exs
@@ -1,8 +1,10 @@
-defmodule ExpediaPhxElmWeb.PageControllerTest do
+defmodule ExpediaPhxElmWeb.OffersControllerTest do
   use ExpediaPhxElmWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  test "GET /api/offers", %{conn: conn} do
+    conn = get conn, "/api/offers"
+    %{"offers" => %{"Hotel" => [ %{"destination" => dest} | _]}} = json_response(conn, 200)
+
+    assert dest["regionID"] == "178286"
   end
 end
