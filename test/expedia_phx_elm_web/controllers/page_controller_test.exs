f87faa8165de9167e803a2f41c5ce6c5965dd261
Saif Elokour <saiffelokour@gmail.com>
2018-01-10 20:18:34 +0200

    adds query test on /api/offers

--- test/expedia_phx_elm_web/controllers/page_controller_test.exs
+++ test/expedia_phx_elm_web/controllers/page_controller_test.exs
@@ -7,4 +7,11 @@ defmodule ExpediaPhxElmWeb.OffersControllerTest do
 
     assert dest["regionID"] == "178286"
   end
+
+  test "GET /api/offers with query on destinationName", %{conn: conn} do
+    conn = get conn, "/api/offers?destinationName=LA"
+    %{"offers" => %{"Hotel" => [ %{"destination" => dest} | _]}} = json_response(conn, 200)
+
+    assert dest["regionID"] == "178280"
+  end
 end
